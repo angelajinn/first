e43d83936d68c05452c20ddd2b1085a1a1aed41a
angela jin <155695803+angelajinn@users.noreply.github.com>
2024-09-16 23:51:14 -0400

    Update recipe_analyzer.sql

--- recipe_analyzer.sql
+++ recipe_analyzer.sql
@@ -1,24 +1,25 @@
--- create table for recipe items
 CREATE TABLE recipe (
 	recipe_name varchar(50),
 	description varchar(50),
 	serving_size int,
 	recipe_type varchar(50),
 	time int,
-	-- create primary key: ensures no two rows in the table have the same value (the column cannot be NULL for primary key)
 	CONSTRAINT recipe_pkey PRIMARY KEY (recipe_name)
 );
 
+-- when creating table name using singular
 CREATE TABLE ingredient (
 	-- surrogate ID: for more complex systems
-	id int GENERATED ALWAYS AS IDENTITY, -- IDENTITY: creates primary key constraint (number value)
+	id int GENERATED ALWAYS AS IDENTITY, -- IDENTITY: creates primary key constraint
 	ingredient_name varchar(50),
 	amount varchar(50),
 	ingredient_type varchar(50),
 	recipe varchar(50),
-	-- foreign key created to link two tables together; also acts as a constraint, ensuring the values in ingredient have a corresponding value in recipe (not unique)
     CONSTRAINT ingredient_recipe_fkey FOREIGN KEY (recipe) REFERENCES recipe (recipe_name)
 );
+
+-- alter a table after the table is created 
+-- alter table ingredients add CONSTRAINT ingredient_pkey PRIMARY KEY (ingredientid)
 
 INSERT INTO Recipe (recipe_name,description,serving_size,recipe_type,time) values ('tomato_pasta', 'italian', 4, 'lunch', 40);
 INSERT INTO Recipe (recipe_name,description,serving_size,recipe_type,time) values ('tteokbokki', 'korean', 2, 'dinner', 20);
@@ -39,20 +40,36 @@ INSERT INTO Ingredient (ingredient_name,amount,ingredient_type,recipe) values ('
 INSERT INTO Ingredient (ingredient_name,amount,ingredient_type,recipe) values ('shrimp', '200g', 'protein', 'yaki_udon');
 INSERT INTO Ingredient (ingredient_name,amount,ingredient_type,recipe) values ('mushroom', '60g', 'vegetable', 'yaki_udon');
 
+select * from ingredient
 
+-- SELECT CLAUSE (what columns) 
 SELECT
   recipe.*,
   ingredient.*
+-- FROM CLAUSE (from what table)
 FROM recipe
+-- JOIN CLAUSE (to what table)
 FULL OUTER JOIN ingredient ON ingredient.recipe=Recipe.recipe_name;
 
 SELECT COUNT(*), recipe_type
 FROM recipe
 GROUP BY recipe_type;
 
--- check how many recipes have the same ingredient type
 -- distinct: if a recipe has 2 vegetable ingredients the recipe will only be counted as 1
 SELECT ingredient.ingredient_type, COUNT(distinct recipe.recipe_name)
 FROM recipe 
 JOIN ingredient ON ingredient.recipe=Recipe.recipe_name
 GROUP BY ingredient_type; 
+
+-- another method
+select ingredient_type, count(distinct recipe_name)
+from (
+    SELECT
+      recipe.*,
+      ingredient.*
+    -- FROM CLAUSE (from what table)
+    FROM recipe
+    -- JOIN CLAUSE (to what table)
+    JOIN ingredient ON ingredient.recipe=Recipe.recipe_name
+) AS JOINED_RESULT
+group by ingredient_type
